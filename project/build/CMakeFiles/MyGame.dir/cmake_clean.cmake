75e2779a9daf176ffa45a5be52c9959dfcda9a54
simichanga <siminischi1234@gmail.com>
2025-03-27 02:25:11 +0200

    simi: refactored a bit

---
+++ project/build/CMakeFiles/MyGame.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGame.dir/link.d"
+  "CMakeFiles/MyGame.dir/main.cpp.o"
+  "CMakeFiles/MyGame.dir/main.cpp.o.d"
+  "CMakeFiles/MyGame.dir/src/assets.cpp.o"
+  "CMakeFiles/MyGame.dir/src/assets.cpp.o.d"
+  "CMakeFiles/MyGame.dir/src/entity.cpp.o"
+  "CMakeFiles/MyGame.dir/src/entity.cpp.o.d"
+  "CMakeFiles/MyGame.dir/src/init.cpp.o"
+  "CMakeFiles/MyGame.dir/src/init.cpp.o.d"
+  "CMakeFiles/MyGame.dir/src/player.cpp.o"
+  "CMakeFiles/MyGame.dir/src/player.cpp.o.d"
+  "MyGame"
+  "MyGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
